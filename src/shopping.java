a25a5c980c34537beb6cc5fd37af7c3abd503ef3
bgwtz4 <b.winston87@yahoo.com>
2019-11-24 19:19:25 -0600

    Formatting Text using \t \b \n

---
+++ src/shopping.java
@@ -0,0 +1,16 @@
+public class shopping {
+    public static void main (String []args){
+        System.out.println("SHOPPING LIST\n\t -Milk\n\t\t > Semi-skimmed\n\t\t > Medium\n\t -Eggs\n\t\t > Free-range\n\t -Bread");
+        cake();
+
+
+
+    }
+
+    public static void cake(){
+        System.out.println("The");
+        System.out.println("\tCake");
+        System.out.println("Is\t\t\tA");
+        System.out.println("\tLie.");
+    }
+}
